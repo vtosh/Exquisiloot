c67d4037f8e90e55e6129fee655458fce32819c4
Vtosh <aeis01@gmail.com>
2021-07-18 17:20:50 +0100

    ScheduleTimer shenanigans because of GUILD_ROSTER_UPDATE event

--- trustBubble.lua
+++ trustBubble.lua
@@ -8,45 +8,51 @@ Exquisiloot.trustedPlayers = {}
 
 local currentShowOffline, guildSize, name, rankName, rankIndex, detected
 function Exquisiloot:buildTrustBubble()
-    GuildRoster()
     currentShowOffline = GetGuildRosterShowOffline()
     SetGuildRosterShowOffline(true)
-    -- Run this as a scheduled task because blizz wants me to mess with the GUILD_ROSTER_UPDATE event
-    Exquisiloot.trustedPlayers = {}
-    Exquisiloot:debug("Building trust bubble for guild rank [%s]", Exquisiloot.db.profile.trustedRank)
     SortGuildRoster("rank")
-    
-    guildSize = GetNumGuildMembers()
-    detected = false
-    for i=1, guildSize, 1 do
-        name, rankName, rankIndex = GetGuildRosterInfo(i)
-        -- Strip server from name
-        Exquisiloot:debug("Currently processing: %s, %s", name, rankName)
-        -- FFS bliz, why does GetGuildRosterInfo return a 0 indexed rank when GuildControlGetRankName is 1 indexed!
-        if (rankIndex+1 > Exquisiloot.db.profile.trustedRank) then
-            -- We've reached the end of our trusted ranks
-            break
-        end
-        name = string.match(name, "(%S+)-%S+")
-        table.insert(Exquisiloot.trustedPlayers, name)
-        if (name == "Vtosh") then
-            detected = true
-        end
-    end
-    -- Make sure i'm included because i'm special
-    if (not detected) then
-        table.insert(Exquisiloot.trustedPlayers, "Vtosh")
-    end
-    if (Exquisiloot:IsDebug()) then
-        for i, v in ipairs(Exquisiloot.trustedPlayers) do
-            print(i, v)
-        end
-    end
+    --if (currentShowOffline) then
+    --    -- changing the show offline didn't cause a GUILD_ROSTER_UPDATE to happen so lets force it
+    --    GuildRoster()
+    --end
 
-    -- Set guild roster back to what it was as much as we can
-    SetGuildRosterShowOffline(currentShowOffline)
-    -- hack to reset the sort, because blizz won't give me directional guild roster sorting
-    SortGuildRoster("name")
+    -- Run this as a scheduled task because blizz wants me to mess with the GUILD_ROSTER_UPDATE event
+    Exquisiloot:ScheduleTimer(function ()
+        Exquisiloot.trustedPlayers = {}
+        Exquisiloot:debug("Building trust bubble for guild rank [%s]", Exquisiloot.db.profile.trustedRank)
+    
+        guildSize = GetNumGuildMembers()
+        detected = false
+        for i=1, guildSize, 1 do
+            name, rankName, rankIndex = GetGuildRosterInfo(i)
+            -- Strip server from name
+            Exquisiloot:debug("Currently processing: %s, %s", name, rankName)
+            -- FFS bliz, why does GetGuildRosterInfo return a 0 indexed rank when GuildControlGetRankName is 1 indexed!
+            if (rankIndex+1 > Exquisiloot.db.profile.trustedRank) then
+                -- We've reached the end of our trusted ranks
+                break
+            end
+            name = string.match(name, "(%S+)-%S+")
+            table.insert(Exquisiloot.trustedPlayers, name)
+            if (name == "Vtosh") then
+                detected = true
+            end
+        end
+        -- Make sure i'm included because i'm special
+        if (not detected) then
+            table.insert(Exquisiloot.trustedPlayers, "Vtosh")
+        end
+        if (Exquisiloot:IsDebug()) then
+            for i, v in ipairs(Exquisiloot.trustedPlayers) do
+                print(i, v)
+            end
+        end
+
+        -- Set guild roster back to what it was as much as we can
+        SetGuildRosterShowOffline(currentShowOffline)
+        -- hack to reset the sort, because blizz won't give me directional guild roster sorting
+        SortGuildRoster("name")
+    end, 1.1)
 end
 
 function Exquisiloot:validateTrust(player)
